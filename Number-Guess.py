86869f094481c4e51050a8009663726f7a2a9e02
Kalyani <kalyani.p.puranik@gmail.com>
2025-08-13 12:47:04 +0000

    commit 3

--- Number-Guess.py
+++ Number-Guess.py
@@ -4,28 +4,29 @@ def number_guess():
     print("Computer is picking a number between 1 and 10")
     n=random.randint(1, 10)
     print("Computer has selected a number")
-    a=0
-    while a!=3:
-        m=int(input("Place Your Guess, You have ",a," Guesses: "))
-        if abs(m-n)>5:
+    a=3
+    while a!=0:
+        print("You have ",a," guesses")
+        m=int(input("Place Your Guess: "))
+        if 10>abs(m-n)>5:
             print("You are not even close lol")
-            a+=1
+            a-=1
         elif abs(m-n)in range (3,6):
             print("Get a little closer")
-            a+=1
-        elif abs(m-n)<3:
+            a-=1
+        elif 1<abs(m-n)<3:
             print("You are very close")
-            a+=1
+            a-=1
         elif abs(m-n)==1:
-            print("You are just one awayy")
-            a+=1
+            print("You are just one away")
+            a-=1
         elif abs(m-n)>=10:
             print("Invalid Input")
-            a+=1
-        elif m==n:
+            a-=1
+        elif int(m)==int(n):
             print ("You got it!!!! Good job")
             break
-    if a==3:
+    if a==0:
         print("You're all out guesses! Better luck next time!")
         print("The number selected by Computer was ",n)
 number_guess()
